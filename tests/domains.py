f200be30ac19ec519c960482bed60d7f63b223d4
Lorenz Stangier <stangier@ukp.informatik.tu-darmstadt.de>
2021-04-03 20:09:43 +0200

    Fix bug that tests dont pause if they fail

--- tests/domains.py
+++ tests/domains.py
@@ -7,112 +7,102 @@ from geizhals import Geizhals
 class TestStringMethods(unittest.TestCase):
     def __init__(self, methodName: str) -> None:
         super().__init__(methodName=methodName)
-        self.SLEEP_TIME = 5
+        self.SLEEP_TIME = 30
 
     # test with ID
     @except_httperror
     def test_ID_AT(self):
-        gh = Geizhals(1688629, "AT")
-        device = gh.parse()
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        gh = Geizhals(1688629, "AT")
+        device = gh.parse()
 
         self.assertEqual(device.name, "Apple iPhone X 64GB grau")
 
     @except_httperror
     def test_ID_EU(self):
-        gh = Geizhals(1688629, "EU")
-        device = gh.parse()
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        gh = Geizhals(1688629, "EU")
+        device = gh.parse()
 
         self.assertEqual(device.name, "Apple iPhone X 64GB grau")
 
     @except_httperror
     def test_ID_DE(self):
-        gh = Geizhals(1688629, "DE")
-        device = gh.parse()
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        gh = Geizhals(1688629, "DE")
+        device = gh.parse()
 
         self.assertEqual(device.name, "Apple iPhone X 64GB grau")
 
     @except_httperror
     def test_ID_UK(self):
-        gh = Geizhals(1688629, "UK")
-        device = gh.parse()
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        gh = Geizhals(1688629, "UK")
+        device = gh.parse()
 
         self.assertEqual(device.name, "Apple iPhone X 64GB grey")
 
     @except_httperror
     def test_ID_PL(self):
-        gh = Geizhals(1688629, "PL")
-        device = gh.parse()
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        gh = Geizhals(1688629, "PL")
+        device = gh.parse()
 
         self.assertEqual(device.name, "Apple iPhone X 64GB szary")
 
     # test with URL
     @except_httperror
     def test_URL_AT(self):
+        # avoid banning from website
+        sleep(self.SLEEP_TIME)
         gh = Geizhals(
             "https://geizhals.at/apple-iphone-x-64gb-grau-a1688629.html", "AT")
         device = gh.parse()
-
-        # avoid banning from website
-        sleep(self.SLEEP_TIME)
 
         self.assertEqual(device.name, "Apple iPhone X 64GB grau")
 
     @except_httperror
     def test_URL_EU(self):
+        # avoid banning from website
+        sleep(self.SLEEP_TIME)
         gh = Geizhals(
             "https://geizhals.eu/apple-iphone-x-64gb-grau-a1688629.html", "EU")
         device = gh.parse()
-
-        # avoid banning from website
-        sleep(self.SLEEP_TIME)
 
         self.assertEqual(device.name, "Apple iPhone X 64GB grau")
 
     @except_httperror
     def test_URL_DE(self):
+        # avoid banning from website
+        sleep(self.SLEEP_TIME)
         gh = Geizhals(
             "https://geizhals.de/apple-iphone-x-64gb-grau-a1688629.html", "DE")
         device = gh.parse()
-
-        # avoid banning from website
-        sleep(self.SLEEP_TIME)
 
         self.assertEqual(device.name, "Apple iPhone X 64GB grau")
 
     @except_httperror
     def test_URL_UK(self):
+        # avoid banning from website
+        sleep(self.SLEEP_TIME)
         gh = Geizhals(
             "https://cenowarka.pl/apple-iphone-x-64gb-szary-a1688629.html", "UK")
         device = gh.parse()
-
-        # avoid banning from website
-        sleep(self.SLEEP_TIME)
 
         self.assertEqual(device.name, "Apple iPhone X 64GB grey")
 
     @except_httperror
     def test_URL_PL(self):
+        # avoid banning from website
+        sleep(self.SLEEP_TIME)
         gh = Geizhals(
             "https://cenowarka.pl/apple-iphone-x-64gb-szary-a1688629.html", "PL")
         device = gh.parse()
-
-        # avoid banning from website
-        sleep(self.SLEEP_TIME)
 
         self.assertEqual(device.name, "Apple iPhone X 64GB szary")
 
--- tests/id2url.py
+++ tests/id2url.py
@@ -11,51 +11,46 @@ class TestStringMethods(unittest.TestCase):
 
     @except_httperror
     def test_URL_AT(self):
-        id = geizhals._url2id(
-            "https://geizhals.at/bose-quietcomfort-35-ii-schwarz-a1696985.html")
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        id = geizhals._url2id(
+            "https://geizhals.at/bose-quietcomfort-35-ii-schwarz-a1696985.html")
 
         self.assertEqual(id, "1696985")
 
     @except_httperror
     def test_URL_EU(self):
-        id = geizhals._url2id(
-            "https://geizhals.eu/bose-quietcomfort-35-ii-schwarz-a1696985.html")
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        id = geizhals._url2id(
+            "https://geizhals.eu/bose-quietcomfort-35-ii-schwarz-a1696985.html")
 
         self.assertEqual(id, "1696985")
 
     @except_httperror
     def test_URL_DE(self):
-        id = geizhals._url2id(
-            "https://geizhals.de/bose-quietcomfort-35-ii-schwarz-a1696985.html")
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        id = geizhals._url2id(
+            "https://geizhals.de/bose-quietcomfort-35-ii-schwarz-a1696985.html")
 
         self.assertEqual(id, "1696985")
 
     @except_httperror
     def test_URL_UK(self):
-        id = geizhals._url2id(
-            "https://skinflint.co.uk/bose-quietcomfort-35-ii-black-a1696985.html")
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        id = geizhals._url2id(
+            "https://skinflint.co.uk/bose-quietcomfort-35-ii-black-a1696985.html")
 
         self.assertEqual(id, "1696985")
 
     @except_httperror
     def test_URL_PL(self):
-        id = geizhals._url2id(
-            "https://cenowarka.pl/bose-quietcomfort-35-ii-czarny-a1696985.html")
-
         # avoid banning from website
         sleep(self.SLEEP_TIME)
+        id = geizhals._url2id(
+            "https://cenowarka.pl/bose-quietcomfort-35-ii-czarny-a1696985.html")
 
         self.assertEqual(id, "1696985")
 
